7c3ade7b2096569946edf64bed4c9e454c8ed5e5
Kelly Stuard <kellystuard@gmail.com>
2024-03-26 04:31:09 +0000

    Adding locals for showing operations

--- .terraform.lock.hcl
+++ .terraform.lock.hcl
@@ -1,21 +1,56 @@
 # This file is maintained automatically by "terraform init".
 # Manual edits may be lost in future updates.
 
-provider "registry.terraform.io/hashicorp/null" {
-  version = "3.2.2"
+provider "registry.terraform.io/hashicorp/azurerm" {
+  version     = "3.97.1"
+  constraints = "~> 3.97"
   hashes = [
-    "h1:zT1ZbegaAYHwQa+QwIFugArWikRJI9dqohj8xb0GY88=",
-    "zh:3248aae6a2198f3ec8394218d05bd5e42be59f43a3a7c0b71c66ec0df08b69e7",
-    "zh:32b1aaa1c3013d33c245493f4a65465eab9436b454d250102729321a44c8ab9a",
-    "zh:38eff7e470acb48f66380a73a5c7cdd76cc9b9c9ba9a7249c7991488abe22fe3",
-    "zh:4c2f1faee67af104f5f9e711c4574ff4d298afaa8a420680b0cb55d7bbc65606",
-    "zh:544b33b757c0b954dbb87db83a5ad921edd61f02f1dc86c6186a5ea86465b546",
-    "zh:696cf785090e1e8cf1587499516b0494f47413b43cb99877ad97f5d0de3dc539",
-    "zh:6e301f34757b5d265ae44467d95306d61bef5e41930be1365f5a8dcf80f59452",
-    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
-    "zh:913a929070c819e59e94bb37a2a253c228f83921136ff4a7aa1a178c7cce5422",
-    "zh:aa9015926cd152425dbf86d1abdbc74bfe0e1ba3d26b3db35051d7b9ca9f72ae",
-    "zh:bb04798b016e1e1d49bcc76d62c53b56c88c63d6f2dfe38821afef17c416a0e1",
-    "zh:c23084e1b23577de22603cff752e59128d83cfecc2e6819edadd8cf7a10af11e",
+    "h1:klBuN2uVZF7AVMhskbbgF8pygyhPBxsjedB1GUV79PA=",
+    "zh:15171efcc3aa3a37748c502c493cb16ecff603b81ada4499a843574976bac524",
+    "zh:2ca6c13a4a96f67763ecced0015c7b101ee02d54ea54b28a8df4ae06468071b1",
+    "zh:2e3c77dbfd8f760132ecef2d6117e939cbea26b96aba5e4d926e7f7f0f7afe72",
+    "zh:4bc346eece1622be93c73801d8256502b11fd7c2e7f7cea12d048bb9fc9fe900",
+    "zh:4f1042942ed8d0433680a367527289459d43b0894a51eaba83ac414e80d5187f",
+    "zh:63e674c31482ae3579ea84daf5b1ba066ce40cb23475f54e17b6b131320a1bec",
+    "zh:8327148766dcb7a174673729a832c8095d7e137d0e6c7e2a9a01da48b8b73fbe",
+    "zh:851b3ae417059a80c7813e7f0063298a590a42f056004f2c2558ea14061c207e",
+    "zh:ac081b48907139c121a422ae9b1f40fc72c6aaaeb05cbdbf848102a6a5f426f4",
+    "zh:dc1d663df2d95e4ba91070ceb20d3560b6ea5c465d39c57a5979319302643e41",
+    "zh:ed26457367cbbb94237e935d297cb31b5687f9abf697377da0ee46974480db9b",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/tfe" {
+  version     = "0.53.0"
+  constraints = "~> 0.53"
+  hashes = [
+    "h1:lqooQSx57bwMOuvns5Hv5XA5JBku1ejvDJmlkJQynPw=",
+    "zh:3d3a751947c16e22b9b925d971bc4522319dffbcf9017eb9efeff69aed2d085f",
+    "zh:4d35d756963d9840061f891252c1f2067678d7c631c31cd91038b9ed7d16e41f",
+    "zh:62b10a45da81fcf103fa12fb9ec466262ea7e1eb78cdfad23a7ad938375764e5",
+    "zh:91be34b76536628f640e7119e1591f51492802fd08112a723f107f3be363c0a0",
+    "zh:b6d9064462247c3797eb2a8e353c93b68c4a40d7dcea79f9815f052e31c21b03",
+    "zh:d5468f2a729c0c23fd4a1cfdd0c2bdda3cbfd6022463a407c9f10251d4d6de3f",
+    "zh:d7ef1a216dd87e7873c0174c4c612813e2abae8e8d1d0447cac203ce4b8972a6",
+    "zh:d95ec293fa70e946b6cd657912b33155f8be3413e6128ed2bfa5a493f788e439",
+    "zh:dfb5c8a8f561ef521f485b87305b78f88d42575444c9ed4b30b38dc0c3a00abf",
+    "zh:e0a34de3045a163144ba33032ce5c5249dbfa5bcb76e54b5bcf55bc4a1f890b8",
+    "zh:f56366b0d5f3a8ce1041884021c5cfccc7216983b9832eaf19b9829eb1fc97c6",
+    "zh:f9718129dfebaa9ab936d0a61d90658822d3982054b5f0a38864f1312a9a1f72",
+  ]
+}
+
+provider "registry.terraform.io/spacelift-io/spacelift" {
+  version     = "1.10.0"
+  constraints = "1.10.0"
+  hashes = [
+    "h1:9BEDHxL2qjSnJ+GsrULlhJBK0Mnf6BptxZLzQk6oNsw=",
+    "zh:2b66f4b973658b2b57f56f768b60a8e7a8729b8bff57a5989e462bb5f68e56c9",
+    "zh:47b1cbb88294535d3a2f7a27a86e885ed493444a5ade5097726f7657824e8fd9",
+    "zh:6c611262c272b2ca8fbdb0813840648bc8e76a3f83f0b1101bc5f4c4ab5ee28e",
+    "zh:6e63d2d5164b6e92f755548fb7a2c59bec59e3b716fe6774f765b2abd78acef9",
+    "zh:8198902d470eb286f09216455a19ac9209dc9cdce85f216ac7205a83b553bad0",
+    "zh:918423410778112ee4763164231cdf62e3d8c094c9c076d48430580dd476df4f",
   ]
 }
--- main.tf
+++ main.tf
@@ -1,6 +1,137 @@
-data "azurerm_resource_group" "zone" {
-  name     = var.zone_resource_group_name
+##
+# This area is used to define the hubs and spokes. Changes to these settings 
+# are part of normal operations to onboard, configure, and deprovision 
+# applications and services.
+#
+# All applications and their environments are defined in `local.applications`.
+# They are flattened and denormalized into `local.application_environments`.
+# From there, they are used to create all the boilerplate resources that allow
+# application self-service.
+##
+
+locals {
+  ##
+  # Statically determines if local resources marked as `cold_resource` should be 
+  # provisioned.
+  ##
+  cold_are_provisioned = false
+
+  ##
+  # This area is used to define the applications and environments. It is created 
+  # at the request of the application owners and updated as they need additional 
+  # environments or metadata about the applicaiton changes.
+  ##
+  applications = {
+    exampleapp1 = {
+      cost_center = "47216" # default cost center for all environments
+      owner       = "jdoe@example.com"
+      environments = {
+        development = {
+          name = "Example Application 1 Development"
+        }
+        test = {
+          cost_center = "47217" # special cost center for test environment
+          name        = "Example Application 1 Test"
+        }
+      }
+    }
+  }
+
+  ##
+  # This area is used to define the services and environments. It is created 
+  # at the request of the service owners and updated as they need additional 
+  # environments or metadata about the service changes.
+  ##
+  services = {
+    authentication = {
+      cost_center = "88149"
+      owner       = "bill.q.service@example.com"
+      name        = "Authentication Service"
+      environments = {
+        development = {
+        }
+        test = {
+        }
+        production = {
+          # overrides default hubs to deploy to all active producion hubs
+          hubs = [for key, value in local.hubs.production : key]
+        }
+      }
+    }
+  }
+
+  hubs = {
+    development = {
+      active_primary = {
+        azure_location = "eastus"
+      }
+    }
+    test = {
+      active_primary = {
+        azure_location = "eastus"
+      }
+      active_secondary = {
+        azure_location = "westus"
+      }
+    }
+    production = {
+      active_primary = {
+        azure_location = "eastus"
+      }
+      active_secondary = {
+        azure_location = "westus"
+      }
+      active_tertiary = {
+        azure_location = "centralus"
+      }
+      disaster_primary = {
+        azure_location = "eastus2"
+        cold_resource  = true
+      }
+    }
+  }
+
+  ##
+  # This area is used to define the defaults for applications and environments. If 
+  # not specified at the application or environment level, these become the values 
+  # used. Environment > Application > Environment Default > Application Default
+  ##
+  application_defaults = {
+    terraform_version = "1.4.6"
+    azure_location    = "eastus"
+    environments = {
+      development = {
+        branch = "main"
+        hubs   = ["active_primary"]
+      }
+      test = {
+        branch = "test"
+        hubs   = ["active_primary"]
+      }
+      production = {
+        branch = "production"
+        hubs   = ["active_primary", "disaster_primary"]
+      }
+    }
+  }
+
+  ##
+  # This area is used to merge the applications with defaults and put it in a format 
+  # more easily consumable by Terraform resources.
+  ##
+  application_environments = { for ae in flatten([
+    for appk, appv in local.applications : [
+      # application-environment > application > application-environment defaults > application defaults
+      for envk, envv in appv.environments : merge(local.application_defaults, local.application_defaults.environments[envk], appv, envv, {
+        app          = appk
+        env          = envk
+        environments = null # an environment should not have visibility to the other environments of its app
+      })
+    ]
+  ]) : "${ae.app}-${ae.env}" => ae }
 }
 
-resource "null_resource" "test" {
+# resource group created by master; used for zone-specific resources
+data "azurerm_resource_group" "zone" {
+  name = var.zone_resource_group_name
 }
