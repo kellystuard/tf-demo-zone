aa8b82c3d5779edb0ddab744ce11f3fcb0d99e13
Kelly Stuard <kellystuard@gmail.com>
2024-03-26 00:30:30 +0000

    Adding resource group test

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-data "azurerm_resource_group" "zone" {
-  name     = var.zone_resource_group_name
+resource "azurerm_resource_group" "zone" {
+  name     = "$(var.zone_resource_group_name)-test"
   location = var.zone_resource_group_location
 }
 
