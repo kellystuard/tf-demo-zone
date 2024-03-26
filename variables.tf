9cb17d2cebcbb951747c681f00ce90b1f4164f41
Kelly Stuard <kellystuard@gmail.com>
2024-03-26 00:18:01 +0000

    Setting up input variables

---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "terraform_provider" {
+  type        = string
+  description = "Provider of Terraform Orchestration. Valid values for terraform_provider are (spacelift, terraform)."
+
+  validation {
+    condition     = contains(["spacelift", "terraform"], var.terraform_provider)
+    error_message = "Valid values for terraform_provider are (spacelift, terraform)."
+  }
+}
+
+variable "resource_prefix" {
+  type        = string
+  description = "Prefix to use for unique resources, such as workspace and resource group names."
+
+  default = "tf-demo-"
+}
+
+variable "zone_resource_group_name" {
+  type        = string
+  description = "Name of zone resource group."
+}
+
+variable "zone_resource_group_location" {
+  type        = string
+  description = "Location of zone resource group."
+}
